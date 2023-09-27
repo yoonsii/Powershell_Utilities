d3d3c15ac4c3be868f1c0b6c3b66af62eaf66976
yoonsii <118307793+yoonsii@users.noreply.github.com>
2023-09-27 20:35:52 +1300

    Cleans variable names and adds header

--- concept-moveFiles-dateArrange.ps1
+++ concept-moveFiles-dateArrange.ps1
@@ -1,16 +1,25 @@
-﻿# Define the Identify-Video function
+#########################################################################################
+#  Author: KP - Yoonsi
+#  Date: 07-09-2023 
+#  Purpose: Recursively searches through a folder and retrieves all photos and videos
+#  Then sorts them in a destination folder depending on created date
+#
+#########################################################################################
+
+
+# Define the Identify-Video function
 function Identify-Video {
     param (
-        $FilePathee
+        $FilePath
     )
 
     #.mp4 .jpg .JPEG .MOV .JPG .PNG .mov .MP4 .GIF
 
-    if ($FilePathee.extension.toString() -eq ".jpg" -or $FilePathee.extension.toString() -eq ".jpeg" -or $FilePathee.extension.toString() -eq ".png") {
-        Write-Host $FilePathee.fullname
-        $imagePath = $FilePathee.fullname
+    if ($FilePath.extension.toString() -eq ".jpg" -or $FilePath.extension.toString() -eq ".jpeg" -or $FilePath.extension.toString() -eq ".png") {
+        Write-Host $FilePath.fullname
+        $imagePath = $FilePath.fullname
         Write-Host "Image!!"
-        Write-Host $FilePathee.dateTaken
+        Write-Host $FilePath.dateTaken
 
         # Load the image using System.Drawing.Bitmap
         $bitmap = New-Object System.Drawing.Bitmap($imagePath)
@@ -30,31 +39,35 @@ function Identify-Video {
         $splitdate = $dateTakenString -split ":"
         $year = $splitdate[0]
         $month = $splitdate[1]
-        $year
-        $month
+        #$year
+        #$month
 
-      # Copy-Item -Path $FilePathee.fullname -Destination C:\New Folder\$year\$monthsFolders[[int]$month -1]
+      # Copy-Item -Path $FilePath.fullname -Destination C:\New Folder\$year\$monthsFolders[[int]$month -1]
         $monthString = $monthsFolders[$month - 1]
 
         if ($dateTakenString -ne $null) {
-            Write-host "Destination " + "C:\New Folder\$year\$monthString"
+            Write-host "Destination "  "$destinationPath\$year\$monthString"
+            move-item -path $FilePath.fullname -destination "$destinationPath\$year\$monthString"
+        } else {
+            Write-host "No date taken so moving to misc"
+            move-item -path $FilePath.fullname -destination "$destinationPath\Misc"
         }
 
     } 
     
     else 
     {
-        Write-Host $FilePathee.fullname
+        Write-Host $FilePath.fullname
         Write-Host "Not image"
         # Specify the path to your file
         
 
         # Use the Get-Item cmdlet to get file information
-        $file = get-item $FilePathee.fullname
+        $file = get-item $FilePath.fullname
         Write-host $file.extension
 
         # Check if the file has media creation date metadata
-        if ($file.Extension -match ".(mp4|avi|mkv|mp3)$") {
+        if ($file.Extension -match ".(mp4|avi|mkv|mp3|mov)$") {
             # Use the Shell.Application COM object to access the metadata
             $shell = New-Object -ComObject Shell.Application
             $folder = $shell.Namespace($file.Directory.FullName)
@@ -72,7 +85,7 @@ function Identify-Video {
             $year
             $month
 
-          # Copy-Item -Path $FilePathee.fullname -Destination C:\New Folder\$year\$monthsFolders[[int]$month -1]
+          # Copy-Item -Path $FilePath.fullname -Destination C:\New Folder\$year\$monthsFolders[[int]$month -1]
           
              $year = $year -match '\d{4}' | Out-Null
             $year = $matches[0]         
@@ -87,12 +100,17 @@ function Identify-Video {
             $monthString = $monthsFolders[$monthInt - 1]
 
             if ($mediaCreatedDate -ne $null) {
-                Write-host "Destination " + "C:\New Folder\$year\$monthString"
+                Write-host "Destinatisdsdffson11 " + "$destinationPath\$year\$monthString"
+                Move-Item -Path $FilePath.fullname -Destination "$destinationPath\$year\$monthString"
+            } else {
+                Write-Host "Media Created Null moving to Misc"
+                Move-Item -Path $FilePath.fullname -Destination "C:\Destination\Misc"
             }
 
 
         } else {
             Write-Host "Media created date metadata not found or file type not supported."
+            
         }
 
     }
@@ -117,22 +135,42 @@ function createFolders {
     }
 }
 
-# Rest of your script below this point
 
 $monthsFolders = "01 - January", "02 - February", "03 - March", "04 - April", "05 - May", "06 - June", "07 - July", "08 - August", "09 - September", "10 - October", "11 - November", "12 - December"
 
-$sourcePath = "C:\source"
-$images = Get-Childitem -Path $sourcePath -Recurse 
-#$images = Get-Childitem -Path $sourcePath -Recurse | foreach-object {$_.extension}
+$sourcePath = Read-Host -Prompt 'Enter Source Path. If left blank will default to C:\Source'
+if ($sourcePath -eq "") 
+{
+    $sourcePath = "C:\Source"
+}
 
-$unique = $images | Select-Object -Unique
+$destinationPath = Read-Host -Prompt 'Enter Destination Path. If left blank will default to C:\Destination'
+if ($destinationPath -eq "") 
+{
+    $destinationPath = "C:\Destination"
+}
+
+$debug = Read-Host -Prompt "Turn verbose on? (Y/N)"
+
+$uniqueFlag = Read-Host -Prompt "Run extension scan only? (Y/N)"
+
+#TO DO: Add regex checking to validate file path
+
+$images = Get-Childitem -Path $sourcePath -Recurse 
+
+if($uniqueFlag -eq "Y") {
+    $images = Get-Childitem -Path $sourcePath -Recurse | foreach-object {$_.extension}
+    $unique = $images | Select-Object -Unique
+    $unique
+    exit
+}
 
 Write-Host $unique
 
 #write-host $images
 
 foreach ($image in $images) {
-   Identify-Video -FilePathee $image
+   Identify-Video -FilePath $image
 }
 
 
