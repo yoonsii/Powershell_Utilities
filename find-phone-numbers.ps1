e1bc97c2ca486718affd2011283b27fa0fca1e9e
yoonsii <118307793+yoonsii@users.noreply.github.com>
2023-10-11 13:40:20 +1300

    Create find-phone-numbers.ps1
    Powershell script to search through a text file using regular expressions and identify phone numbers in a limited set of format types.
    
    (123) 456-7890
    123-456-7890
    123.456.789

---
+++ find-phone-numbers.ps1
@@ -0,0 +1,22 @@
+$path = read-host -prompt "Enter Path"
+#Naive regex pattern but it works for now. See if you can tidy this up / make it more efficient
+
+#Hard-Coded for ease of development
+#$path = "C:\sandbox\phone\phone.txt"
+
+foreach ($line in  (Get-Content -Path $path)){
+
+    #Write-Host $line
+    #Write-Host "`n"
+
+    #\d{1,5}-\d{1,5}-\d{1,5}
+
+    $matches = $line | Select-String -allmatches -pattern "\(?\d{1,5}\)?\.?\s?-?\d{1,5}-?\.?\d{1,5}"
+
+        foreach ($match in $matches){
+          write-host $match.Matches
+        }
+    
+
+
+}
